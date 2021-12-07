40db4e95c4847f4627cc06aa7ac00b6551a3f16a
정주봉 <wnqhdqhd123@gmail.com>
2021-12-08 00:37:00 +0900

    https://programmers.co.kr/learn/courses/30/lessons/42576 정답

--- src/HashPractice.java
+++ src/HashPractice.java
@@ -3,17 +3,32 @@ import java.util.HashMap;
 public class HashPractice {
     //    완주하지 못한 선수
     //    https://programmers.co.kr/learn/courses/30/lessons/42576
+
+
     public String Question1(String[] participant, String[] completion){
         String answer = "";
-        HashMap<String, Integer> hm = new HashMap<>();
-        for (String player : participant) hm.put(player, hm.getOrDefault(player, 0) + 1);
-        for (String player : completion) hm.put(player, hm.get(player) - 1);
 
-        for (String key : hm.keySet()) {
-            if (hm.get(key) != 0){
-                answer = key;
+        HashMap<String, Integer> hashParticipant = new HashMap<>();
+
+        for (String s : completion) {
+            if (hashParticipant.containsKey(s)) {
+                hashParticipant.put(s, hashParticipant.get(s) + 1);
+            } else {
+                hashParticipant.put(s, 0);
             }
         }
+        for (String s : participant) {
+            int result = hashParticipant.getOrDefault(s, -1);
+            if (result == -1) {
+                answer = s;
+                break;
+            } else if (result == 0) {
+                hashParticipant.remove(s);
+            } else {
+                hashParticipant.put(s, hashParticipant.get(s) - 1);
+            }
+        }
+
         return answer;
     }
 }
