56104b78d55dc02463bd0647013b3c877460de31
정주봉 <wnqhdqhd123@gmail.com>
2021-12-08 01:09:26 +0900

    전화번호 목록 1차 시도 효율성 테스트 3,4 실패

--- src/HashPractice.java
+++ src/HashPractice.java
@@ -3,8 +3,6 @@ import java.util.HashMap;
 public class HashPractice {
     //    완주하지 못한 선수
     //    https://programmers.co.kr/learn/courses/30/lessons/42576
-
-
     public String Question1(String[] participant, String[] completion){
         String answer = "";
 
@@ -26,6 +24,27 @@ public class HashPractice {
                 hashParticipant.remove(s);
             } else {
                 hashParticipant.put(s, hashParticipant.get(s) - 1);
+            }
+        }
+
+        return answer;
+    }
+
+//    전화번호 목록
+//    https://programmers.co.kr/learn/courses/30/lessons/42577
+    public boolean Question2(String[] phone_book) {
+        boolean answer = true;
+
+        loop:
+        for (int i = 0; i < phone_book.length; i++) {
+            for (int j = 0; j < phone_book.length; j++) {
+                if(i == j) continue;
+
+                if (phone_book[i].length() <= phone_book[j].length() &&
+                        phone_book[i].equals(phone_book[j].substring(0,phone_book[i].length()))) {
+                    answer = false;
+                    break loop;
+                }
             }
         }
 
--- test/HashPracticeTest.java
+++ test/HashPracticeTest.java
@@ -1,6 +1,10 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import javax.xml.crypto.dsig.keyinfo.KeyValue;
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class HashPracticeTest {
@@ -24,5 +28,16 @@ class HashPracticeTest {
         assertEquals(result1, hashPractice.Question1(part1, comp1));
         assertEquals(result2, hashPractice.Question1(part2, comp2));
         assertEquals(result3, hashPractice.Question1(part3, comp3));
+    }
+
+    @Test
+    void 전화번호_목록() {
+        String[] phonebook1 = {"119", "97674223", "1195524421"};
+        String[] phonebook2 = {"123","456","789"};
+        String[] phonebook3 = {"12","123","1235","567","88"};
+
+        assertEquals(false, hashPractice.Question2(phonebook1));
+        assertEquals(true , hashPractice.Question2(phonebook2));
+        assertEquals(false, hashPractice.Question2(phonebook3));
     }
 }
